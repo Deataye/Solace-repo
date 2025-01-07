472b299c26d3c2d60759aa4c1f9ef443d1be1950
Deataye <shahzaiba61@gmail.com>
2025-01-07 12:11:01 +0500

    checking again

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Navbar from "./components/Navbar";
-import HomePage from "./pages/Homepage"; // Ensure this file exists
-import ServicesPage from "./pages/ServicesPage"; // Ensure this file exists
+import Navbar from "./components/Navbar.jsx";
+import HomePage from "./pages/HomePage.jsx"; // Ensure this file exists
+import ServicesPage from "./pages/ServicesPage.jsx"; // Ensure this file exists
 
 function App() {
   return (
--- src/pages/ServicesPage.jsx
+++ src/pages/ServicesPage.jsx
@@ -1,7 +1,7 @@
 import DiscoverSection from "../components/DiscoverSection";
 import FAQSection from "../components/FAQSection";
 import Footer from "../components/Footer";
-import Navbar from "../components/Navbar";
+
 import SaleSection from "../components/SaleSection";
 import TopCategories from "../components/TopCategories";
 
@@ -9,7 +9,7 @@ import TopCategories from "../components/TopCategories";
 function ServicesPage() {
   return (
     <>
-      {/* <Navbar /> */}
+    
        <DiscoverSection/>
        <TopCategories/>
        <SaleSection/>
